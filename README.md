06b5e52b60d7169b7ad0ae3d4dc7a063f4ff3624
Yasmin <yasmin27101991@gmail.com>
2023-11-14 18:28:24 +0100

    create endpoints

---
+++ README.md
@@ -0,0 +1,31 @@
+# Owen Wilson ha dicho 'wow'
+Esta página web es el resultado de la evaluación final del módulo 3:React de Adalab.
+## ENUNCIADO:
+ 1.  Listado de escenas:
+> El ejercicio consiste en desarrollar una página web con el listado de las escenas de las películas donde el actor Owen Wilson ha dicho 'wow', con una API que nos devuelve información de 50 escenas de películas aleatorias. Sobre cada una, vamos a mostrar al menos:
+> 
+*  Poster (poster)
+*  Película (movie)
+*  Frase completa (full_line)
+*  Año (year)
+1.  Filtrado de pelicula y año:
+> La segunda parte consiste en poder buscarlos por pelicula. Para ello, añadimos un input a la interfaz, de forma que al ir escribiendo queden en la intefaz.
+> Para el año, añadimos un select.
+1.  Componentes del listado de personajes
+El listado debe tener los siguientes componentes como mínimo:
+* Componente para los filtros.
+* Componente para el listado (MovieSceneList). 
+* Componente para la tarjeta de cada escena del listado (MovieSceneItem).
+* Componente para el detalle de cada escena del listado (MovieSceneDetail).
+
+1. Detalle de cada escena
+> Inplementando una nueva funcionalidad: al hacer clic sobre la tarjeta de una escena, su información
+aparecerá a pantalla completa. Para hacer esto usaremos rutas y React Router DOM. En la pantalla de
+   
+## TECNOLOGÍA EMPLEADA:
+ > HTML y CSS para la maquetación.
+ > JavaScript y React para la interacción con el usuario.
+## PASOS PARA ARRANCAR EL PROYECTO:
+1.  Clonar el repositorio.
+2.  Ejecutar en la terminal *npm install* para instalar las dependencias necesarias.
+3.  Ejecutar en la terminal *npm start*  para arrancar el servidor.
--- src/index.js
+++ src/index.js
@@ -25,3 +25,120 @@ app.listen(PORT, () => {
 
 //ENDPOINTS
 
+// Obtener todos los libros
+app.get('/books', async (req, res) => {
+  let query = 'SELECT * FROM books';
+
+  const conn = await getConnection();
+
+  const [results] = await conn.query(query);
+  const numOfElements = results.length;
+
+    res.json({
+      info: { count: numOfElements }, 
+      results: results,
+    });
+  });
+
+
+// Obtener un libro por ID
+app.get('/books/:id', async (req, res) => {
+  const bookId = req.params.id;
+
+ let query = 'SELECT * FROM books WHERE id = ?';
+ const conn = await getConnection();
+
+ const [results] = await conn.query(query, [bookId]);
+ const numOfElements = results.length;
+
+ if (numOfElements === 0) {
+  res.json({
+    success: true,
+    message: "No existe el libro que buscas",
+  });
+  return;
+}
+  res.json({
+    results: results[0], // listado
+    });
+  });
+
+
+// Crear un nuevo libro
+app.post('/books', async (req, res) => {
+  const dataBook = req.body;
+  const { title, author, genre, publication_year } = dataBook;
+
+  let sql = 'INSERT INTO books (title, author, genre, publication_year) VALUES (?, ?, ?, ?)';
+
+  try {
+    const conn = await getConnection();
+
+    const [results] = await conn.query(sql ,[
+      title, 
+      author, 
+      genre, 
+      publication_year 
+    ]);
+  
+    if (results.affectedRows === 0) {
+      res.json({
+        success: false,
+        message: "No se ha podido insertar",
+      });
+      return;
+    }
+
+    res.json({
+      success: true,
+      id: results.insertId,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: `Ha ocurrido un error${error}`,
+    });
+  }
+});
+
+// Actualizar un libro por ID
+app.put('/books/:id', async (req, res) => {
+  const dataBook = req.body;
+  const { title, author, genre, publication_year } = dataBook;
+  const bookId = req.params.id;
+  
+  let sql = 'UPDATE books SET title=?, author=?, genre=?, publication_year=? WHERE id=?';
+
+  const conn = await getConnection();
+
+    const [results]= await conn.query(sql, [
+      title, 
+      author, 
+      genre, 
+      publication_year, 
+      bookId
+    ]);
+    res.json({
+      success: true,
+      message: "Actualizado correctamente",
+    });
+  });
+
+// Eliminar un libro por ID
+app.delete('/books/:id', async (req, res) => {
+  const bookId = req.params.id;
+
+  let sql = 'DELETE FROM books WHERE id = ?';
+
+  const conn = await getConnection();
+  
+  const [results] = await conn.query(sql, [bookId]);
+  res.json({
+    success: true,
+    message: "Eliminado correctamente",
+  });
+});
+
+
+
+
